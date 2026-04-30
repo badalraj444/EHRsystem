a8eeb6ebb9648dd24d67639c2486305d3816d71c
badalraj444@gmail.com <badalraj444@gmail.com>
2026-04-30 11:02:46 +0530

    Remove outdated client

--- client/src/utils/blockchain.js
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-
-const provider = new ethers.JsonRpcProvider(import.meta.env.VITE_BESU_RPC_URL);
-
-export const getAccounts = async () => {
-    const accounts = await provider.listAccounts();
-    return accounts;
-};
